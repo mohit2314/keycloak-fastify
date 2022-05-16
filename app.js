a652674ebffbf438bc3d2d18fa3638f3c71c3f92
mohit2314 <mohitpatel2314@gmail.com>
2022-05-17 00:35:22 +0530

    init app

---
+++ app.js
@@ -0,0 +1,24 @@
+const fastify = require("fastify")({ logger: true });
+// fastify.register(require('./plugin/mongo'))
+fastify.register(require("@fastify/swagger"), {
+  exposeRoute: true,
+  routePrefix: "/docs",
+  swagger: {
+    info: { title: "keycloak-fastify" },
+  },
+  consumes: ['application/x-www-form-urlencoded']
+});
+// fastify.register(require("./routes/items"));
+
+const PORT = 5000;
+
+const start = async () => {
+  try {
+    await fastify.listen(PORT);
+  } catch (error) {
+    fastify.log.error(error);
+    process.exit(1);
+  }
+};
+
+start();
